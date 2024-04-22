089b6b10528a475060381e83ddf04114d80e5818
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-04-22 18:20:23 +0100

    TypeScript - arrays + enums

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -10,5 +10,21 @@ level = ''
 console.log(level);
 
 function render(document: any) {
-    console.log(document);; 
-}
+    console.log(document);;
+};
+
+let numbers: number[] = [1, 2, 3]; // in this case we could skip the type of the variable
+let numbers2: number[] = []; // in this case there is a need of explicit annotation of the type (otherwise it will be any)
+
+
+// enums (PascalCase)
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+const enum Size { Small = 1, Medium, Large };
+
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
